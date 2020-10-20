b5eb4a9974318dc35eab1d0021e0f516eeb1547c
Jonas Buntinx <buntinxjonas@gmail.com>
2020-10-20 16:26:15 +0900

    Updated the Root

--- src/App/Root.purs
+++ src/App/Root.purs
@@ -2,18 +2,42 @@ module App.Root where
 
 import Prelude
 
+import Data.Maybe (Maybe(..))
 import React.Basic.DOM as R
+import React.Basic.Events (handler_)
 import React.Basic.Hooks as React
 import React.Halo as Halo
 
+data Action
+  = Reply
+
 makeRoot :: React.Component Unit
 makeRoot = do
-  Halo.component "Root" { init, render, eval: Halo.makeEval Halo.defaultEval }
-  where
-  init = { greeting: "Before this is over, promise me you'll figure out which one of us is the machine." }
-
-  render { state } =
-    R.div
-      { children:
-        [ R.text state.greeting ]
+  Halo.component "Root"
+      { init
+      , render
+      , eval: Halo.makeEval $ Halo.defaultEval { onAction = handleAction }
       }
+  where
+  init = { reply: Nothing }
+
+  handleAction = case _  of
+    Reply -> Halo.modify_ _ { reply = Just "Sir. Finishing this fight." }
+
+  render { state, send } =
+    React.fragment
+      [ R.div
+        { children:
+          [ R.text "Master Chief, mind telling me what you're doing on that ship?"
+          ]
+        }
+      , case state.reply of
+          Nothing ->
+            R.button
+              { onClick: handler_ $ send Reply
+              , children:
+                [ R.text "Reply"
+                ]
+              }
+          Just reply -> R.div { children: [ R.text reply ] }
+      ]
